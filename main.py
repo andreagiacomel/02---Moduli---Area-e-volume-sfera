f3efde3a810fed2065da16f5477aafd000a73fb7
andrea-giacomel <andrea.giacomel.05@gmail.com>
2020-01-22 14:49:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import sfera
+
+raggio = 10
+
+print(sfera.area(raggio))
+print(sfera.volume(raggio))
---
+++ sfera.py
@@ -0,0 +1,6 @@
+def area(raggio=1):
+	return 2 * 3.14 * raggio**2
+
+
+def volume(raggio=1):
+	return 4 * 3.14 * raggio**3
